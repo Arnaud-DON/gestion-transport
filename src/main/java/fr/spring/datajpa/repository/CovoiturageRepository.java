b50ff7326f409fd4c26d5a570398dd34b94dc7e6
Arnaud DON <donnadille.arnaud@gmail.com>
2021-09-01 10:00:09 +0200

    Add query findAvailableCovoitsForUserID

--- src/main/java/fr/spring/datajpa/repository/CovoiturageRepository.java
+++ src/main/java/fr/spring/datajpa/repository/CovoiturageRepository.java
@@ -1,12 +1,22 @@
 package fr.spring.datajpa.repository;
-import fr.spring.datajpa.model.Covoiturage;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
+import fr.spring.datajpa.model.Covoiturage;
 
 public interface CovoiturageRepository extends JpaRepository<Covoiturage, Long> {
     @Query(value = "SELECT cv FROM Covoiturage cv JOIN FETCH cv.passagers psgr WHERE psgr.mail=:mail", nativeQuery = false)
     public List<Covoiturage> findCovoiturageCollaborateur(@Param("mail") String mail);
+
+    @Query(value = "SELECT cv FROM Covoiturage cv JOIN FETCH cv.passagers psgr"
+    			+ " WHERE cv.TravelStatus = 'CREATED'"
+    			+ " AND cv.organisateur.id != :userId"
+    			+ " AND (SELECT count(c2) FROM Covoiturage c2"
+					+ " WHERE psgr.id = :userId) <= 0"
+			, nativeQuery = false)
+    public List<Covoiturage> findAvailableCovoitsForUserID(@Param("userId") Long userId);
+
 }
