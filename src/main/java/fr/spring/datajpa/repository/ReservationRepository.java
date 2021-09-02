078a98a482e5717a99ecbc97d8e2550bb05154f7
Arnaud DON <donnadille.arnaud@gmail.com>
2021-09-02 15:15:58 +0200

    resolve import

--- src/main/java/fr/spring/datajpa/repository/ReservationRepository.java
+++ src/main/java/fr/spring/datajpa/repository/ReservationRepository.java
@@ -1,10 +1,12 @@
 package fr.spring.datajpa.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
+import fr.spring.datajpa.model.Reservation;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
     @Query(value = "SELECT resa FROM Reservation resa JOIN FETCH resa.organisateur orga"
